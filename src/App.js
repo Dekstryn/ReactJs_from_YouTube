964e4d8e414f6756c6bfb9493f1034ef6c926e2f
Krzysztof Nyrek <krzysztof.nyrek@gmail.com>
2019-09-09 21:10:00 +0200

    Added component Message to App

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from './components/greet'
 import Welcome from './components/welcome'
+import Message from './components/Message'
 class App extends Component {
   render(){
     return (
       <div className="App">
         <Greet name="Bruce"/>
-        <Greet name="Clark"/>
-        <Greet name="Diana"/>
         <Welcome name="Bruce"/>
+        <Message />
       </div>
     );
   }
