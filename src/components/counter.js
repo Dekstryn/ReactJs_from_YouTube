0503b0b37ccd3405dcc28f08dafbf18e3614ed4c
Krzysztof Nyrek <krzysztof.nyrek@gmail.com>
2019-09-10 05:19:24 +0200

    Fixed bug with no content display

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-class counter extends Component {
+class Counter extends Component {
   render() {
     return (
       <div>
@@ -10,4 +10,4 @@ class counter extends Component {
   }
 }
 
-export default counter
+export default Counter
