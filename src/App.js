224b58833896e9d911f69a98c6beb3e1e1c41ab2
Krzysztof Nyrek <krzysztof.nyrek@gmail.com>
2019-09-10 05:16:18 +0200

    Set import counter components

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import './App.css';
 import Greet from './components/greet'
 import Welcome from './components/welcome'
 import Message from './components/Message'
+import counter from './components/counter'
+
 class App extends Component {
   render(){
     return (
       <div className="App">
-        <Greet name="Bruce"/>
+        <counter />
+        {/*<Greet name="Bruce"/>
         <Welcome name="Bruce"/>
-        <Message />
+        <Message />*/}
       </div>
     );
   }
