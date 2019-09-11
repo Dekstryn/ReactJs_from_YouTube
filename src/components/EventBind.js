102d0aabbb92502bc1cababfe6c0f96d592adc42
Krzysztof Nyrek <krzysztof.nyrek@gmail.com>
2019-09-11 06:20:40 +0200

    Add constructor

--- src/components/EventBind.js
+++ src/components/EventBind.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react'
 
 class EventBind extends Component {
+  
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+     message: "Hello" 
+    }
+  }
+  
   render() {
     return (
       <div>
