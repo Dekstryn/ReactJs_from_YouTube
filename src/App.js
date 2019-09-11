08adb3ac2cf8074ef628bbe5a66a07c109b88bd3
Krzysztof Nyrek <krzysztof.nyrek@gmail.com>
2019-09-11 06:05:39 +0200

    Import ClassClick component to App

--- src/App.js
+++ src/App.js
@@ -6,13 +6,14 @@ import Welcome from './components/welcome'
 import Message from './components/Message'
 import Counter from './components/counter'
 import FunctionClick from './components/FunctionClick'
+import ClassClick from './components/ClassClick'
 
 class App extends Component {
   render(){
     return (
       <div className="App">
-        <FunctionClick />
-        {/*
+        <ClassClick />
+        {/*<FunctionClick />
         <Counter />
         <Greet name="Bruce"/>
         <Welcome name="Bruce"/>
