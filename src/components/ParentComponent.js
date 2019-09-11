29a9d3715ad16b4eab6d982373084dc3bf62906f
Krzysztof Nyrek <krzysztof.nyrek@gmail.com>
2019-09-11 06:51:19 +0200

    Added constructor

--- src/components/ParentComponent.js
+++ src/components/ParentComponent.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react'
 
 class ParentComponent extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       parentName: "Parent"
+    }
+  }
+  
   render() {
     return (
       <div>
