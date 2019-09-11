533177cbf88ba79d64a4c7b1069c1c7598de564c
Krzysztof Nyrek <krzysztof.nyrek@gmail.com>
2019-09-11 06:02:53 +0200

    Import component FunctionClick

--- src/App.js
+++ src/App.js
@@ -5,13 +5,16 @@ import Greet from './components/greet'
 import Welcome from './components/welcome'
 import Message from './components/Message'
 import Counter from './components/counter'
+import FunctionClick from './components/FunctionClick'
 
 class App extends Component {
   render(){
     return (
       <div className="App">
+        <FunctionClick />
+        {/*
         <Counter />
-        {/*<Greet name="Bruce"/>
+        <Greet name="Bruce"/>
         <Welcome name="Bruce"/>
         <Message />*/}
       </div>
