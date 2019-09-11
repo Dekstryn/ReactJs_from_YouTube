15cf57c3986ce0bc8f24d6dab7258e86ece78766
Krzysztof Nyrek <krzysztof.nyrek@gmail.com>
2019-09-11 06:14:39 +0200

    Set onClick event

--- src/components/ClassClick.js
+++ src/components/ClassClick.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
 
 class ClassClick extends Component {
+  
+  clickHandler(){
+    console.log('Clicked the button')
+  }
+
   render() {
     return (
       <div>
-        <button>Click me</button>
+        <button onClick={this.clickHandler}>Click me</button>
       </div>
     )
   }
